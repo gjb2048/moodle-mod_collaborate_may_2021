c436c342a25f6a692da49b9b6460f97ec2f7c380
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2021-05-09 18:48:59 +0100

    Real task 1!

--- lang/en/collaborate.php
+++ lang/en/collaborate.php
@@ -48,3 +48,6 @@ $string['nocollaborates'] = '';
 // Mod form specific collaborate settings.
 $string['title'] = 'Activity title: ';
 $string['title_help'] = 'Please enter a suitable title for your collaborate main page';
+
+// Week 2 Task 1.
+$string['pageviewed'] = 'Page viewed';
